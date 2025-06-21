d3dec0d6c3903c3b8e81c8bbbb86f960584d066c
frmdstryr <frmdstryr@protonmail.com>
2025-06-20 20:37:03 -0400

    Update expected

--- test/expected/Test.cxx
+++ test/expected/Test.cxx
@@ -33,7 +33,7 @@ PYBIND11_MODULE(Test, mod) {
 
 
 // ENUM: TAGGEDENUM
-py::enum_<TaggedEnum>(mod, "TaggedEnum", "None")
+py::enum_<TaggedEnum>(mod, "TaggedEnum", "")
 	.value("TaggedEnum_A", TaggedEnum::TaggedEnum_A)
 	.value("TaggedEnum_B", TaggedEnum::TaggedEnum_B)
 	.export_values();
@@ -53,9 +53,9 @@ py::class_<Test_SimpleClass> cls_Test_SimpleClass(mod, "Test_SimpleClass", "Test
 cls_Test_SimpleClass.def(py::init<>());
 
 // Methods
-cls_Test_SimpleClass.def("TestReturnPolicy1", (int (Test_SimpleClass::*)()) &Test_SimpleClass::TestReturnPolicy1, "None");
-cls_Test_SimpleClass.def("TestReturnPolicy2", (const int & (Test_SimpleClass::*)()) &Test_SimpleClass::TestReturnPolicy2, "None");
-cls_Test_SimpleClass.def("TestReturnPolicy3", (int & (Test_SimpleClass::*)()) &Test_SimpleClass::TestReturnPolicy3, "None", py::return_value_policy::reference_internal);
+cls_Test_SimpleClass.def("TestReturnPolicy1", (int (Test_SimpleClass::*)()) &Test_SimpleClass::TestReturnPolicy1, "");
+cls_Test_SimpleClass.def("TestReturnPolicy2", (const int & (Test_SimpleClass::*)()) &Test_SimpleClass::TestReturnPolicy2, "");
+cls_Test_SimpleClass.def("TestReturnPolicy3", (int & (Test_SimpleClass::*)()) &Test_SimpleClass::TestReturnPolicy3, "", py::return_value_policy::reference_internal);
 
 // After type
 // Testing +after_type line 1
@@ -66,30 +66,30 @@ cls_Test_SimpleClass.def("TestReturnPolicy3", (int & (Test_SimpleClass::*)()) &T
 // TYPEDEF: UNTAGGEDENUM
 
 // CLASS: TEST_GETTER
-py::class_<Test_Getter> cls_Test_Getter(mod, "Test_Getter", "None");
+py::class_<Test_Getter> cls_Test_Getter(mod, "Test_Getter", "");
 
 // Constructors
 cls_Test_Getter.def(py::init<>());
 
 // Methods
-cls_Test_Getter.def("OtherValue", (int & (Test_Getter::*)()) &Test_Getter::OtherValue, "None");
-cls_Test_Getter.def("Value", (int & (Test_Getter::*)()) &Test_Getter::Value, "None", py::return_value_policy::reference_internal);
-cls_Test_Getter.def("SetValue", (int (Test_Getter::*)(int)) &Test_Getter::SetValue, "None", py::arg("v"));
+cls_Test_Getter.def("OtherValue", (int & (Test_Getter::*)()) &Test_Getter::OtherValue, "");
+cls_Test_Getter.def("Value", (int & (Test_Getter::*)()) &Test_Getter::Value, "", py::return_value_policy::reference_internal);
+cls_Test_Getter.def("SetValue", (int (Test_Getter::*)(int)) &Test_Getter::SetValue, "", py::arg("v"));
 
 // CLASS: TEST_NODE
-py::class_<Test_Node> cls_Test_Node(mod, "Test_Node", "None");
+py::class_<Test_Node> cls_Test_Node(mod, "Test_Node", "");
 
 // Constructors
 cls_Test_Node.def(py::init<>());
 
 // CLASS: TEST_MESH
-py::class_<Test_Mesh> cls_Test_Mesh(mod, "Test_Mesh", "None");
+py::class_<Test_Mesh> cls_Test_Mesh(mod, "Test_Mesh", "");
 
 // Constructors
 cls_Test_Mesh.def(py::init<>());
 
 // Methods
-cls_Test_Mesh.def("AddNode", (void (Test_Mesh::*)(const int, Test_Node*)) &Test_Mesh::AddNode, "None", py::arg("id"), py::arg("node"), py::keep_alive<1, 2>());
+cls_Test_Mesh.def("AddNode", (void (Test_Mesh::*)(const int, Test_Node*)) &Test_Mesh::AddNode, "", py::arg("id"), py::arg("node"), py::keep_alive<1, 2>());
 
 // CLASS: TEST_PNAME
 py::class_<Test_Pname> cls_Test_NewName(mod, "Test_NewName", "Test class");
